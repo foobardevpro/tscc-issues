e2b7b1a954f63a27c72eec1ea22c81d53707bae0
foobardevpro <55914344+foobardevpro@users.noreply.github.com>
2019-10-26 05:06:44 +0300

    fix

--- src/root.ts
+++ src/root.ts
@@ -9,7 +9,7 @@ two issues (these two issues might be related to each other)
 ------------------------------------------------------------
 1. Run watch.js
 2. Notice nothing was bundled (was supposed to create multiple bundles inside /dist) (react external seems to be causing this issue)
-3. Comment out the console log at the bottom of this file, and save -- now the watch does bundle, BUT, it bundles all modules into a single file! (names it bundle2.js)
+3. Comment out the react import above, and save -- now the watch does bundle, BUT, it bundles all modules into a single file! (names it bundle2.js)
 
 
 last issue
@@ -22,5 +22,3 @@ last issue
 I'm not sure how you should deal with the last issue but it deserves mentioning, especially because you're bound to receive it if you use rollup.
 
 */
-
-console.log(React)
